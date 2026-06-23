327a69570b43b05a1deb868f0395b0bca777ece2
Khrystyna <pankiv38@gmail.com>
2026-06-23 20:37:14 +0100

    SCRUM-7: Create UI for child login

---
+++ app/(auth)/kid-login.tsx
@@ -0,0 +1,7 @@
+import KidLoginUI from "@/features/auth/components/kid/KidLoginUI";
+
+export default function KidLogin() {
+  return (
+      <KidLoginUI />
+  );
+}
---
+++ components/ui/GridOverlay.tsx
@@ -0,0 +1,142 @@
+import StarIcon from "@/assets/svg-icons/StarIcon";
+import { useAppColors } from "@/hooks/use-app-colors";
+import { fullScreenHeight } from "@/lib/constants";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+
+const gridSize = 24;
+
+const starsLayer: {
+  icon: React.ReactNode;
+  style: StyleProp<ViewStyle>;
+}[] = [
+  {
+    icon: <StarIcon />,
+    style: {
+      left: "10%",
+      top: "15%",
+    },
+  },
+  {
+    icon: <StarIcon color="#00D4FF" />,
+    style: {
+      left: "70%",
+      top: "25%",
+      transform: [{ rotate: "20deg" }],
+    },
+  },
+  {
+    icon: <StarIcon color="#00D4FF" />,
+    style: {
+      left: "30%",
+      bottom: "15%",
+      transform: [{ rotate: "10deg" }],
+    },
+  },
+  {
+    icon: <StarIcon />,
+    style: {
+      right: "10%",
+      bottom: "25%",
+    },
+  },
+];
+
+export default function GridOverlay({
+  width,
+  height = fullScreenHeight,
+  inset = 0,
+  withStars,
+}: {
+  width: number;
+  height?: number;
+  inset?: number;
+  withStars?: boolean;
+}) {
+  const colors = useAppColors();
+
+  const verticalLines = Array.from({
+    length: Math.ceil(width / gridSize) + 1,
+  });
+  const horizontalLines = Array.from({
+    length: Math.ceil(height / gridSize) + 1,
+  });
+
+  const dynamicStyles = StyleSheet.create({
+    gridLineVertical: {
+      backgroundColor: colors.white,
+    },
+    gridLineHorizontal: {
+      backgroundColor: colors.white,
+    },
+  });
+
+  return (
+    <>
+      {withStars && (
+        <View style={[styles.absolute, StyleSheet.absoluteFill]}>
+          {starsLayer.map((star, index) => (
+            <View key={index} style={[styles.absolute, star.style]}>
+              {star.icon}
+            </View>
+          ))}
+        </View>
+      )}
+
+      <View
+        pointerEvents="none"
+        style={[
+          styles.gridOverlay,
+          {
+            left: inset,
+            top: inset,
+            width,
+            height,
+          },
+        ]}
+      >
+        {verticalLines.map((_, index) => (
+          <View
+            key={`v-${index}`}
+            style={[
+              styles.gridLineVertical,
+              dynamicStyles.gridLineVertical,
+              { left: index * gridSize },
+            ]}
+          />
+        ))}
+        {horizontalLines.map((_, index) => (
+          <View
+            key={`h-${index}`}
+            style={[
+              styles.gridLineHorizontal,
+              dynamicStyles.gridLineHorizontal,
+              { top: index * gridSize },
+            ]}
+          />
+        ))}
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridOverlay: {
+    position: "absolute",
+    opacity: 0.12,
+  },
+  gridLineVertical: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    width: 1,
+  },
+  gridLineHorizontal: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    height: 1,
+  },
+  absolute: {
+    position: "absolute",
+  },
+});
