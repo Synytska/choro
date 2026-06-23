1c36f8b32e6479aa87480bef5ec015a03fdfbd21
Khrystyna <pankiv38@gmail.com>
2026-06-23 09:55:06 +0100

    Add new icons and app layout
    Add localization setup
    
    Create custom Input and Button
    
    Add zod ckeck and loginForm Component
    
    SCRUM-5: Create UI for parent login screen

---
+++ features/auth/components/parent/ParentLoginUI.tsx
@@ -0,0 +1,73 @@
+import ParentIcon from "@/assets/svg-icons/ParentIcon";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { useTranslation } from "react-i18next";
+import { ScrollView, StyleSheet, View } from "react-native";
+import LoginForm from "./LoginForm";
+import { useAppColors } from "@/hooks/use-app-colors";
+
+export default function ParentLoginUI() {
+  const { t } = useTranslation();
+  const colors = useAppColors();
+
+  const dynamicStyles = StyleSheet.create({
+    iconWrapper: {
+      backgroundColor: colors.lightGrey,
+    },
+    subtitle: {
+      color: colors.darkGrey,
+    },
+  });
+
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContent}>
+      <ThemedView style={styles.container}>
+        <ThemedView style={[styles.iconWrapper, dynamicStyles.iconWrapper]}>
+          <ParentIcon color={colors.black} style={styles.icon} />
+        </ThemedView>
+        <View style={styles.textWrapper}>
+          <ThemedText style={styles.header}>{t("parentAccess")}</ThemedText>
+          <ThemedText style={[styles.subtitle, dynamicStyles.subtitle]}>
+            {t("parentSubtitle")}
+          </ThemedText>
+        </View>
+        <ThemedView style={styles.form}>
+          <LoginForm />
+        </ThemedView>
+      </ThemedView>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollContent: {
+    flexGrow: 1,
+    paddingTop: 50,
+  },
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 48,
+  },
+  iconWrapper: {
+    borderRadius: 50,
+    padding: 22,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+  },
+  textWrapper: {
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 600,
+  },
+  subtitle: {
+    fontSize: 14,
+  },
+  form: {
+    alignSelf: "stretch",
+  },
+});
